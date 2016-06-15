b27f26a70a6ffae3cfa17aca67e0f70e26816fc5
melissajoseph <meljoseph12@gmail.com>
2016-06-15 13:58:37 -0700

    Update qpm.c

--- qpm.c
+++ qpm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
 
 int main()
@@ -32,7 +33,7 @@ int main()
 
         for (int nn=0; nn<nobj; ++nn) {
             for (int idim=0; idim<3; ++idim) {
-                P[ntotal+nn].pos[idim] = fbuf[3*nn+idim];
+                P[nn].pos[idim] = fbuf[3*nn+idim];
             }
         }
 
@@ -42,6 +43,76 @@ int main()
 
         for (int nn=0; nn<nobj; ++nn) {
             for (int idim=0; idim<3; ++idim) {
-                P[ntotal+nn].vel[idim] = fbuf[3*nn+idim];
+                P[nn].vel[idim] = fbuf[3*nn+idim];
             }
         }
+        
+//Read the density        
+     float rhomin=1e30, rhomax=-1e30;
+     fread(&buff[0],sizeof(float),nobj,fp);
+
+     for (int nn=0; nn<nobj; ++nn) {
+          float d_in = buff[nn];
+          float d_out;
+          d_out = sqrt(
+                           (RhoClip*RhoClip) * (d_in*d_in)
+                         /
+                           ( RhoClip*RhoClip + d_in*d_in  )
+                        );
+          if (d_out < rhomin) rhomin=d_out;
+          if (d_out > rhomax) rhomax=d_out;
+
+          P[nn].rho = d_out;
+        }
+        printf("Rhomin,Rhomax %4.2f %4.2f \n",rhomin,rhomax);
+        printf("Number of particles %d \n", nobj);
+        fclose(fname);
+        return 0;
+
+}
+
+
+double selectprob(const float rho) {
+double prob;
+double mu=1.0;
+if (rho>0){
+double x = (log(rho)-mu)/0.1;
+prob = exp(-0.5*x*x);
+}
+else {
+prob = 0.0;
+}
+return(prob);
+}
+
+
+struct subparticle select_sample(double nbar, struct subparticle P)  {
+
+double navg=0, ntot=0,norm;
+long size = P.size();
+struct subparticle *tmp;
+     tmp = (struct subparticle*) malloc (nbar)
+int nn;
+for (int nn=0; nn < size; ++nn) {
+     double p = P[nn].wt * selectprob(P[nn].rho);
+     if (p > maxprob) maxprob=p;
+          navg += p;
+          ntot += P[nn].wt;
+     }
+
+norm = nbar/navg;
+int c = 0;
+struct subparticle Ptmp;
+          Ptmp = (struct subparticle*) malloc (1);
+for (int nn=0; nn<size; ++nn) {
+     double r = drand48();
+     double prob = norm * P[nn].wt * this->model.selectprob(P[nn].rho) * P[nn].exclude;
+     
+     if((r < prob)) {
+          c += 1;
+          for (int idim=0; idim<3; ++idim) {
+               Ptmp.pos[idim] = P[nn].pos[idim];
+               Ptmp.vel[idim] = P[nn].vel[idim];
+                    }
+          tmp[c] = Ptmp;
+     }
